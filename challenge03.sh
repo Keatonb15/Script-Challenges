970643fca31fd412d29be8ee5e646a2895b7caef
Keaton B <burgesskeaton@outlook.com>
2023-04-26 21:48:15 -0400

    update

---
+++ challenge03.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+#A function that prints the login history of users on this computer
+
+function print_something () {
+echo This is the login history
+last
+}
+print_something
+print_something
+print_something
