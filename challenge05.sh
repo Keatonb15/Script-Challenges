4869b2b44ac3482218baf95ea2c113f8759ab0f0
Keaton B <burgesskeaton@outlook.com>
2023-05-02 20:03:01 -0400

    first push

---
+++ challenge05.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Script: Ops 201 Class 04 Ops Challenge solution
+#Author: Keaton Burgess
+#Date of last revision: 05/02/2023
+#Purpose: Create a loop
+
+y="x"
+
+while [ $y == "x" ]
+do
+    ps aux
+    echo "Choose a PID."
+    read pid
+    kill $pid
+    break
+done
