1693125dd094c813acad7c9956b599f90b990bbc
Keaton B <burgesskeaton@outlook.com>
2023-05-08 20:53:54 -0400

    first push

---
+++ challenge08.bat
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Script: Ops 201 Class 08 Ops Challenge solution
+#Author: Keaton Burgess
+#Date of last revision: 05/08/2023
+#Purpose: Windows Bash Scripting
+
+
+
+@echo off
+
+ROBOCOPY "C:\Users\J.thompson\Desktop\My Project Files" "C:\Users\J.thompson\Documents" /e
+
+pause
+
