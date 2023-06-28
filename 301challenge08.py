ac0da94d9e190b543974095a5b3d981290a20a88
Keaton B <burgesskeaton@outlook.com>
2023-06-27 21:43:53 -0400

    first

---
+++ 301challenge08.py
@@ -0,0 +1,11 @@
+#!/bin/python
+#Script: Ops 301 Class 08 Ops Challenge Solution
+#Author: Keaton Burgess
+#Date of last revision: 06/26/2023
+#Purpose: Python Collections
+
+NBATeams = ["Jazz","Nuggets","Warriors","Heat","Lakers","Suns","Celtics","Bulls","Pistons","Nets"]
+print(NBATeams[3])
+print(NBATeams[5:])
+NBATeams[6] = "onion"
+print(NBATeams)  
